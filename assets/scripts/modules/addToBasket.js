89b35929c23b47989778bd55a7e0f277b27cb3f4
Pavel <bochesan@gmail.com>
2019-04-23 18:12:32 +0300

    Animate add to basket v.2

--- assets/scripts/modules/addToBasket.js
+++ assets/scripts/modules/addToBasket.js
@@ -1,8 +1,13 @@
 ;(function() {
+    var check = true;
     $(document).on('click', '.product-row__button', function() {
-        $(this).addClass("button--check");
-        setTimeout(function() {
-            $(this).removeClass("button--check");
-        }.bind(this), 2000);
+        if (check) {
+            check = false;
+            $(this).addClass("button--check");
+            setTimeout(function() {
+                $(this).removeClass("button--check");
+                check = true;
+            }.bind(this), 2000);
+        }
     });
 })();

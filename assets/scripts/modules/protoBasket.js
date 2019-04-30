ab38155aa8e3480082ec4b5d8c9fed80fb521375
Bochesan <bochesan89@yandex.ru>
2019-04-30 12:59:45 +0300

    method _numAdd and _numDel

--- assets/scripts/modules/protoBasket.js
+++ assets/scripts/modules/protoBasket.js
@@ -1,27 +1,57 @@
 ;(function() {
 
     class basketProduct {
-        constructor(self, priceBlock, numPlus, numMinus, del) {
+        constructor(self, numProducts, numMinus, numPlus, del) {
             if (self === undefined) {
                 return false;
             }
             this._self = self;
-            this._price = this._self.querySelectorAll('.' + priceBlock)[0];
-            this._numPlus = this._self.querySelectorAll('.' + numPlus)[0];
+            this._numProducts = this._self.querySelectorAll('.' + numProducts)[0];
             this._numMinus = this._self.querySelectorAll('.' + numMinus)[0];
+            this._numPlus = this._self.querySelectorAll('.' + numPlus)[0];
             this._del = this._self.querySelectorAll('.' + del)[0];
         }
 
         _delete() {
             let self = this;
+            
             this._del.addEventListener('click', function() {
-                console.log('del');
-                self._self.remove();
+                self._self.classList.add('remove');
+                setTimeout(()=>{
+                    self._self.remove();
+                }, 550);
+            });
+        }
+
+        _numAdd() {
+            let self = this;
+
+            this._numPlus.addEventListener('click', function() {
+                let numProd = +self._numProducts.innerHTML;
+                numProd = numProd + 1;
+                self._numProducts.innerHTML = numProd;
+            });
+        }
+
+        _numDel() {
+            let self = this;
+
+            this._numMinus.addEventListener('click', function() {
+                let numProd = +self._numProducts.innerHTML;
+                numProd = numProd - 1;
+                if (numProd <= 1) {
+                    self._numProducts.innerHTML = 1;
+                }
+                else {
+                    self._numProducts.innerHTML = numProd;
+                }
             });
         }
 
         init() {
             this._delete();
+            this._numAdd();
+            this._numDel();
         }
 
     }

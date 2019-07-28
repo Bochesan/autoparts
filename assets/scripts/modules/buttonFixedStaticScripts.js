f4e047c40fb3b71301388c8849cf96ea38bdbb29
Yaroslav <y.vertuhov@itsoft.ru>
2019-07-28 22:53:29 +0300

    fix buttonFixedStaticScripts

--- assets/scripts/modules/buttonFixedStaticScripts.js
+++ assets/scripts/modules/buttonFixedStaticScripts.js
@@ -2,13 +2,15 @@
     var buttonCart = $(".buttonCart__button"),
         formBuy = $(".delivery");
 
-        $(window).scroll(function(){
-            if ($(this).scrollTop() + $(this).height() >= formBuy.offset().top) {
-                buttonCart.addClass("buttonCart__button--hide");
-            } else {
-                buttonCart.removeClass("buttonCart__button--hide");
-            }
-        });
+        if (formBuy.length) {
+            $(window).scroll(function(){
+                if ($(this).scrollTop() + $(this).height() >= formBuy.offset().top) {
+                    buttonCart.addClass("buttonCart__button--hide");
+                } else {
+                    buttonCart.removeClass("buttonCart__button--hide");
+                }
+            });
+        }
 
         $(".buttonCart").click(function() {
             $('body').animate({ scrollTop: formBuy.offset().top - $("header").height()}, 1100); //1100 - скорость
